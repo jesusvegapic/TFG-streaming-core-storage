3d68d1cf26edea7c4cd72810822a653ddd0d30cf
jesusvegapic <91549721+jesusvegapic@users.noreply.github.com>
2022-01-11 20:38:22 +0100

    Initial Commit config
    Initial config using Spring Initialazr

---
+++ src/main/java/es/jesusvegapic/TFGsteamingcorejava/TfgSteamingCoreJavaApplication.java
@@ -0,0 +1,13 @@
+package es.jesusvegapic.TFGsteamingcorejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TfgSteamingCoreJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TfgSteamingCoreJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/jesusvegapic/TFGsteamingcorejava/TfgSteamingCoreJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package es.jesusvegapic.TFGsteamingcorejava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TfgSteamingCoreJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
